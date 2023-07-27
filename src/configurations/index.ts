5db9b86306d7fd046e8b811bdf0b277b9c97580f
Akem-Ben <akemini.ndaobong@gmail.com>
2023-07-28 00:51:12 +0100

    setup

---
+++ src/configurations/index.ts
@@ -0,0 +1,27 @@
+import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const {DEV_DB_NAME,
+    DEV_DB_USERNAME,
+    DEV_DB_PASSWORD} = process.env
+
+export const db = new Sequelize(
+    DEV_DB_NAME!, // DB_NAME!, //database name
+    DEV_DB_USERNAME!, // DB_USERNAME!, //name of user
+    DEV_DB_PASSWORD!, // DB_PASSWORD!, //db password
+
+    {
+      host: "localhost",
+      port: 5434,
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        encrypt: true,
+        // ssl: {
+        //   rejectUnauthorized: false,
+        // },
+      },
+    }
+  );
